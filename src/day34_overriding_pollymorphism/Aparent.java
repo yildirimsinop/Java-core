6903b8d9a58daa2469e6b3153a8dbba11acf472a
yildirimsinop <norvecoslo57@>
2023-03-20 14:20:25 +0100

    day34 1.time

---
+++ src/day34_overriding_pollymorphism/Aparent.java
@@ -0,0 +1,13 @@
+package day34_overriding_pollymorphism;
+
+public class Aparent {
+
+    public void method1 () {
+
+        System.out.println("parent class method1");
+    }
+    public void method2 () {
+
+        System.out.println("parent class method2");
+    }
+}
---
+++ src/day34_overriding_pollymorphism/BChild.java
@@ -0,0 +1,15 @@
+package day34_overriding_pollymorphism;
+
+public class BChild extends Aparent {
+
+        public void method1() {
+
+            System.out.println("Child class method1");
+        }
+
+        public void method2 (int a) {
+
+            System.out.println("Child class method2");
+        }
+    }
+
