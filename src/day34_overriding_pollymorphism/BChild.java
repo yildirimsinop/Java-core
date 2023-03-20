44e7571dc9caa305e552fbf854b8ed856299f6b1
yildirimsinop <norvecoslo57@>
2023-03-20 16:38:50 +0100

    siste endring 1. time

--- src/day34_overriding_pollymorphism/BChild.java
+++ src/day34_overriding_pollymorphism/BChild.java
@@ -2,14 +2,37 @@ package day34_overriding_pollymorphism;
 
 public class BChild extends Aparent {
 
-        public void method1() {
+    public void method1() {
 
-            System.out.println("Child class method1");
-        }
-
-        public void method2 (int a) {
-
-            System.out.println("Child class method2");
-        }
+        System.out.println("Child class method1");
     }
 
+    public void method2 (int a) {
+
+        // overriding olabilmesi icin
+        // hem method ismi, hem de method signature ayni olmalidir.
+
+        System.out.println("Child class method2");
+    }
+
+    public void method2 (String isim) {
+
+        // Overriding Child ve parents claslari arasinda olur.
+
+        System.out.println("Child class String parametreli method");
+    }
+
+    @Override
+    public void method3(String str) {
+        // super.method3(str);
+        // overriding varsa parent ve child class'daki
+        // overridden ve overriding method'lardan sadece biri calisir.
+        // eger ikisini birden calistirmak isterseniz
+        // super.overriddenMethodIsmi yazilir.
+
+        /*
+           Overrideng method @Override notasyonu ile isaretlenebilir.
+
+         */
+    }
+}
