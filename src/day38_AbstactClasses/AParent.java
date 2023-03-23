0ceebea5aa4802c682e19672bdd971671153ff54
yildirimsinop <norvecoslo57@>
2023-03-23 15:55:59 +0100

    day38 2.time

---
+++ src/day38_AbstactClasses/AParent.java
@@ -0,0 +1,19 @@
+package day38_AbstactClasses;
+
+public class AParent {
+
+    public void method1 () {
+
+        System.out.println("Child class'lar bu method1'i barindirmali ");
+    }
+
+    public void method2 () {
+
+        System.out.println("Child class'lar bu method2'i barindirmali ");
+    }
+
+    public void method3 () {
+
+        System.out.println("Child class'lar bu method3'i barindirmali ");
+    }
+}
---
+++ src/day38_AbstactClasses/BChild.java
@@ -0,0 +1,17 @@
+package day38_AbstactClasses;
+
+public class BChild extends AParent{
+    public static void main(String[] args) {
+
+        /*
+            Bu gune kadar gordugumuz class'larda extends keyword ile bir class'i parent
+            edinirsek parent class'lardaki method'lari ovveride et MECBURIYETIMIZ YOKTUR.
+
+            Istersek parent class'daki tum method'lari override ederiz istersek de
+            parent class'daki tum method'lari kullanir,
+            hic birini override etmeyiz.
+
+         */
+
+    }
+}
